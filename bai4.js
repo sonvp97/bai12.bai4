35d5a1f8c48ad332c359775127c34f26b19d30d9
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-16 11:14:09 +0700

    first commit

---
+++ bai4.js
@@ -0,0 +1,9 @@
+let numbers = [-3,5,1,3,2,10];
+let max = numbers[0];
+let index = 0;
+for (let i = 0;i < numbers.length; i++){
+    if (numbers[i] > max){
+        max = numbers[i];
+        index = i;
+    }
+}alert('Gía trị lớn nhất là' + max + 'giá trị số :'+index);
